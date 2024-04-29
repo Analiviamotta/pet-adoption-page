85d6ca1ecbffadcc765011ec1ed8de6184fa34b9
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-04-29 01:52:01 -0300

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import './Footer.css'
+export const Footer = () => {
+    return(
+        <footer className='footer-container'>
+            <p>footer</p>
+        </footer>
+    )
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import './Header.css'
+export const Header = () => {
+    return(
+        <header className='header-container'>
+            <h1>header</h1>
+        </header>
+    )
+}
