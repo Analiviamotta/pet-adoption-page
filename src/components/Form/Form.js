8760b726d9dac8300563d32f76395beae9013150
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-04-29 09:40:26 -0300

    feat: Add options in Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,27 @@
 import Button from '../Button'
+import ListOptions from '../ListOptions'
 import TextField from '../TextField'
 import './Form.css'
 export const Form = () => {
+    const petAdoptionList = [
+        'Lola',
+        'Nevora',
+        'Frida',
+        'Camões',
+        'Thor',
+        'Atena'
+    ]
     return (
         <section className='form-container'>
             <form>
-                <h1>Deixe sua mensagem aqui e veja o que acontece</h1>
-                <TextField label="nome" placeholder="nome"></TextField>
+                <h1>Deixe sua mensagem para um dos animais disponíveis</h1>
+                <TextField notNull = {true} label="nome" placeholder="nome"></TextField>
                 <TextField label="Mensagem" placeholder="mensagem"></TextField>
-                <Button text = "Enviar"></Button>
+                <ListOptions 
+                    label= "Escolha um animal" 
+                    options= {petAdoptionList}>
+                </ListOptions>
+                <Button>Enviar</Button>
             </form>
         </section>
     )
