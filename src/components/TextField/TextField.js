55da588373474cbfd203da6f0382121bf5b37138
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-04-29 08:55:41 -0300

    fix: Fixing bug in console

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -3,13 +3,9 @@ import './TextField.css'
 export const TextField = (props) => {
     const placeholder = `Digite o seu ${props.placeholder}...`
     return(
-        <div text-field-container>
-            <label className='label'>
-                {props.label}
-            </label>
-
-            <input placeholder = {placeholder} className='input'>
-            </input>
+        <div className='text-field-container'>
+            <label className='label'>{props.label}</label>
+            <input required = {props.notNull} placeholder = {placeholder} className='input'></input>
         </div>
     )
 }
