9d22d0c83a33144f11dce90f2049540bc57ea9a5
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-04-29 04:45:36 -0300

    feat: Add component button

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,14 @@
+import Button from '../Button'
 import TextField from '../TextField'
 import './Form.css'
 export const Form = () => {
     return (
         <section className='form-container'>
-            <h2>Deixe sua mensagem aqui e veja o que acontece</h2>
             <form>
+                <h1>Deixe sua mensagem aqui e veja o que acontece</h1>
                 <TextField label="nome" placeholder="nome"></TextField>
                 <TextField label="Mensagem" placeholder="mensagem"></TextField>
+                <Button text = "Enviar"></Button>
             </form>
         </section>
     )
